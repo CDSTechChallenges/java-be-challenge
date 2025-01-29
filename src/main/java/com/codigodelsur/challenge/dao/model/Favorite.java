9f5c5577b760b6c95722e4f9f204a8e9749d4fd4
Norberto Verino <nverino@codigodelsur.com>
2025-01-29 08:43:09 -0300

    chore: remove unneeded import

--- src/main/java/com/codigodelsur/challenge/dao/model/Favorite.java
+++ src/main/java/com/codigodelsur/challenge/dao/model/Favorite.java
@@ -3,7 +3,6 @@ package com.codigodelsur.challenge.dao.model;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
